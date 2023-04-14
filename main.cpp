6f74722ccf994bc72091a5e4ae97a156fdf4af66
Jocselyn Aguilar <ajocseline@gmail.com>
2023-04-14 17:35:03 -0600

    hashing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "core/header/ui/SfmlApp.h"
 #include "core/header/Map.h"
+#include "core/header/Hash.h"
 
 using namespace std;
 
@@ -21,8 +22,23 @@ void GraphTest() {
 
     map->Print();
 }
+
+
+void testHashing(){
+    Hash *table = new Hash;
+    table->addCity(table->hashing(0), new City("Winterfell", 1, 1));
+    table->addCity(table->hashing(1), new City("Poniente", 3, 3));
+    City *city = table->getCity(table->hashing(0), "winterfell");
+    if (city != nullptr) {
+        cout << city->GetName();
+    } else{
+        cout << "The values was not found";
+    }
+}
+
 int main() {
     SfmlTest();
     //GraphTest();
+    testHashing();
     return 0;
 }
