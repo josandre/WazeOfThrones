518ef6c5b14669c8c28a50d62a6b4a5498c931a9
erocham <erocham@ucenfotec.ac.cr>
2023-04-17 19:48:00 -0600

    Elian: map DAO 3

--- main.cpp
+++ main.cpp
@@ -13,14 +13,13 @@ void SfmlTest() {
 
 void CitiDAOTest(){
     CityDAO citiDao;
-    citiDao.printCities();
 }
 
 void GraphTest() {
     Map* map = new Map();
-    map->AddCity("A", 0, 0);
-    map->AddCity("B", 0, 0);
-    map->AddCity("C", 0, 0);
+    map->AddCity("A", 0, 0, 1);
+    map->AddCity("B", 0, 0, 1);
+    map->AddCity("C", 0, 0, 1);
 
     map->AddRoute("A", "B", 10, 5);
     map->AddRoute("A", "C", 1, 3);
@@ -32,8 +31,8 @@ void GraphTest() {
 
 void testHashing(){
     Hash *table = new Hash;
-    table->addCity(table->hashing(0), new City("Winterfell", 1, 1));
-    table->addCity(table->hashing(1), new City("Poniente", 3, 3));
+  //  table->addCity(table->hashing(0), new City("Winterfell", 1, 1));
+  //  table->addCity(table->hashing(1), new City("Poniente", 3, 3));
     City *city = table->getCity("Poniente");
 
     if (city != nullptr) {
