8d8d04783288ff07a8be48083588f602ad667019
erocham <erocham@ucenfotec.ac.cr>
2023-04-17 19:26:58 -0600

    Elian: map DAO

--- core/source/CityDAO.cpp
+++ core/source/CityDAO.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include "../header/City.h"
+#include "../header/Map.h"
 
 class CityDAO{
 private:
@@ -12,36 +13,37 @@ private:
 
 public:
     CityDAO(){
+        Map* map = new Map();
         // Agregar las ciudades y sus datos
-        cities.push_back(City("Bastion Kar",1.0, 2.0));
-        cities.push_back(City("Invernalia",1.0, 2.0));
-        cities.push_back(City("Torrhen",1.0, 2.0));
-        cities.push_back(City("Puerto Blanco",1.0, 2.0));
-        cities.push_back(City("Varamar",1.0, 2.0));
-        cities.push_back(City("Los Dedos",1.0, 2.0));
-        cities.push_back(City("Nido de Águilas",1.0, 2.0));
-        cities.push_back(City("El Valle",1.0, 2.0));
-        cities.push_back(City("Aguas Dulces",1.0, 2.0));
-        cities.push_back(City("Colmillo Dorado",1.0, 2.0));
-        cities.push_back(City("Cueva",1.0, 2.0));
-        cities.push_back(City("Harrenhal",1.0, 2.0));
-        cities.push_back(City("Astas",1.0, 2.0));
-        cities.push_back(City("Puerto Gaviota",1.0, 2.0));
-        cities.push_back(City("Roca Casterly",1.0, 2.0));
-        cities.push_back(City("Lannisport",1.0, 2.0));
-        cities.push_back(City("El Dominio",1.0, 2.0));
-        cities.push_back(City("Ladera",1.0, 2.0));
-        cities.push_back(City("Senda Hueso",1.0, 2.0));
-        cities.push_back(City("Aguas Negras",1.0, 2.0));
-        cities.push_back(City("Desembarco del Rey",1.0, 2.0));
-        cities.push_back(City("Bosque Alto",1.0, 2.0));
-        cities.push_back(City("Bastion de Tormentas",1.0, 2.0));
-        cities.push_back(City("Alto Jardín",1.0, 2.0));
-        cities.push_back(City("Antigua",1.0, 2.0));
-        cities.push_back(City("Asperon",1.0, 2.0));
-        cities.push_back(City("Campo Estrella",1.0, 2.0));
-        cities.push_back(City("Lanza del Sol",1.0, 2.0));
-        cities.push_back(City("Yalth",1.0, 2.0));
+        map->AddCity("Bastion Kar",1.0, 2.0);
+        map->AddCity("Invernalia",1.0, 2.0);
+        map->AddCity("Torrhen",1.0, 2.0);
+        map->AddCity("Puerto Blanco",1.0, 2.0);
+        map->AddCity("Varamar",1.0, 2.0);
+        map->AddCity("Los Dedos",1.0, 2.0);
+        map->AddCity("Nido de Águilas",1.0, 2.0);
+        map->AddCity("El Valle",1.0, 2.0);
+        map->AddCity("Aguas Dulces",1.0, 2.0);
+        map->AddCity("Colmillo Dorado",1.0, 2.0);
+        map->AddCity("Cueva",1.0, 2.0);
+        map->AddCity("Harrenhal",1.0, 2.0);
+        map->AddCity("Astas",1.0, 2.0);
+        map->AddCity("Puerto Gaviota",1.0, 2.0);
+        map->AddCity("Roca Casterly",1.0, 2.0);
+        map->AddCity("Lannisport",1.0, 2.0);
+        map->AddCity("El Dominio",1.0, 2.0);
+        map->AddCity("Ladera",1.0, 2.0);
+        map->AddCity("Senda Hueso",1.0, 2.0);
+        map->AddCity("Aguas Negras",1.0, 2.0);
+        map->AddCity("Desembarco del Rey",1.0, 2.0);
+        map->AddCity("Bosque Alto",1.0, 2.0);
+        map->AddCity("Bastion de Tormentas",1.0, 2.0);
+        map->AddCity("Alto Jardín",1.0, 2.0);
+        map->AddCity("Antigua",1.0, 2.0);
+        map->AddCity("Asperon",1.0, 2.0);
+        map->AddCity("Campo Estrella",1.0, 2.0);
+        map->AddCity("Lanza del Sol",1.0, 2.0);
+        map->AddCity("Yalth",1.0, 2.0);
     }
 
     // Obtener ciudades por índice
