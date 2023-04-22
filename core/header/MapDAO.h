55922da6a02763d9ac8377cee7a7ae85ff7b5f6f
Taylor <taylor.segura@fairplaylabs.com>
2023-04-22 14:20:12 -0600

    Located cities in the correct position

--- core/header/MapDAO.h
+++ core/header/MapDAO.h
@@ -12,36 +12,39 @@ class MapDAO {
 public:
     Map* InitMap() {
         Map* map = new Map();
+
+        // Cities
         map->AddCity("BASTION KAR", 1200.0, 180.0, 0);
         map->AddCity("INVERNALIA", 910.0, 270.0, 1);
         map->AddCity("TORRHEN", 660.0, 440.0, 2);
         map->AddCity("PUERTO BLANCO", 970.0, 520.0, 3);
         map->AddCity("VARAMAR", 900.0, 830.0, 4);
-        map->AddCity("LOS DEDOS", 1.0, 2.0, 5);
-        map->AddCity("Nido de Águilas",1.0, 2.0, 6);
-        map->AddCity("El Valle",1.0, 2.0, 7);
-        map->AddCity("Aguas Dulces",1.0, 2.0, 8);
-        map->AddCity("Colmillo Dorado",1.0, 2.0, 9);
-        map->AddCity("Cueva",1.0, 2.0, 10);
-        map->AddCity("Harrenhal",1.0, 2.0, 11);
-        map->AddCity("Astas",1.0, 2.0, 12);
-        map->AddCity("Puerto Gaviota",1.0, 2.0, 13);
-        map->AddCity("Roca Casterly",1.0, 2.0, 14);
-        map->AddCity("Lannisport",1.0, 2.0, 15);
-        map->AddCity("El Dominio",1.0, 2.0, 16);
-        map->AddCity("Ladera",1.0, 2.0, 17);
-        map->AddCity("Senda Hueso",1.0, 2.0, 18);
-        map->AddCity("Aguas Negras",1.0, 2.0, 19);
-        map->AddCity("Desembarco del Rey",1.0, 2.0, 20);
-        map->AddCity("Bosque Alto",1.0, 2.0, 21);
-        map->AddCity("Bastion de Tormentas",1.0, 2.0, 22);
-        map->AddCity("Alto Jardín",1.0, 2.0, 23);
-        map->AddCity("Antigua",1.0, 2.0, 24);
-        map->AddCity("Asperon",1.0, 2.0, 25);
-        map->AddCity("Campo Estrella",1.0, 2.0, 26);
-        map->AddCity("Lanza del Sol",1.0, 2.0, 27);
-        map->AddCity("Yalth",1.0, 2.0, 28);
+        map->AddCity("LOS DEDOS", 1110.0, 790.0, 5);
+        map->AddCity("NIDO DE AGUILAS",1240.0, 870.0, 6);
+        map->AddCity("EL VALLE",1015.0, 940.0, 7);
+        map->AddCity("AGUAS DULCES",860.0, 985.0, 8);
+        map->AddCity("COLMILLO DORADO",715.0, 1050.0, 9);
+        map->AddCity("CUEVA",825.0, 1190.0, 10);
+        map->AddCity("HARRENHAL",940.0, 1105.0, 11);
+        map->AddCity("ASTAS",1100.0, 1140.0, 12);
+        map->AddCity("PUERTO GAVIOTA",1150.0, 1000.0, 13);
+        map->AddCity("ROCA CASTERLY",675.0, 1180.0, 14);
+        map->AddCity("LANNISPORT",640.0, 1320.0, 15);
+        map->AddCity("EL DOMINIO",790.0, 1430.0, 16);
+        map->AddCity("LADERA",945.0, 1375.0, 17);
+        map->AddCity("SENDA HUESO",1015.0, 1480.0, 18);
+        map->AddCity("Aguas Negras",920.0, 1260.0, 19);
+        map->AddCity("DESEMBARCO DEL REY",1030.0, 1310.0, 20);
+        map->AddCity("BOSQUE ALTO",1130.0, 1400.0, 21);
+        map->AddCity("BASTION DE TORMENTAS",1120.0, 1520.0, 22);
+        map->AddCity("ALTO JARDIN",840.0, 1580.0, 23);
+        map->AddCity("ANTIGUA",680.0, 1680.0, 24);
+        map->AddCity("ASPERON",830.0, 1815.0, 25);
+        map->AddCity("CAMPO ESTRELLA",925.0, 1730.0, 26);
+        map->AddCity("LANZA DEL SOL",1130.0, 1770.0, 27);
+        map->AddCity("YALTH",1100.0, 1835.0, 28);
 
+        // Routes
         map->AddRoute("BASTION KAR", "INVERNALIA", 10, 5);
         map->AddRoute("BASTION KAR", "PUERTO BLANCO", 2, 8);
         map->AddRoute("PUERTO BLANCO", "INVERNALIA", 1, 8);
--- main.cpp
+++ main.cpp
@@ -42,9 +42,7 @@ void CitiDAOTest(){
 
 
 int main() {
-
     //CitiDAOTest();
-
     SfmlTest();
     //GraphTest();
 
